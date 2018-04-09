ca227ce334eb320abacc812a66ef9dbf9716531e
alexxayeko <ayekoalexx@gmail.com>
2018-04-09 10:00:02 +0100

    first file

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sample</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link href="vendors/animate/animate.css" rel="stylesheet">
+    <!-- Icon CSS-->
+	<link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <!-- Camera Slider -->
+    <link rel="stylesheet" href="vendors/camera-slider/camera.css">
+    <!-- Owlcarousel CSS-->
+	<link rel="stylesheet" type="text/css" href="vendors/owl_carousel/owl.carousel.css" media="all">
+
+    <!--Theme Styles CSS-->
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Preloader -->
+    <div class="preloader"></div>
+
+	<!-- Top Header_Area -->
+	<section class="top_header_area">
+	    <div class="container">
+            <ul class="nav navbar-nav top_nav">
+                <li><a href="#"><i class="fa fa-star"></i></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right social_nav">
+               <li style="color:#fff;">
+                <?php 
+                    echo date('Y-m-d H:i:s');
+
+               ?></li>
+            </ul>
+	    </div>
+	</section>
+	<div class="container">
+  <div class="jumbotron">
+    <h1 style="margin-left: 100px;">First, you were not to use any frameworks. This is Bootstrap. Second, preview this page now. It should show current date. Thanks</h1> 
+    <p style="width: 600px; margin-left: 200px; ">
+        <img src="images/hello.png" class="img-thumbnail" alt="Cinque Terre">
+    </p> 
+  </div>
+</div
+
+
+    <!-- jQuery JS -->
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Animate JS -->
+    <script src="vendors/animate/wow.min.js"></script>
+    <!-- Camera Slider -->
+    <script src="vendors/camera-slider/jquery.easing.1.3.js"></script>
+    <script src="vendors/camera-slider/camera.min.js"></script>
+    <!-- Isotope JS -->
+    <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+    <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+    <!-- Progress JS -->
+    <script src="vendors/Counter-Up/jquery.counterup.min.js"></script>
+    <script src="vendors/Counter-Up/waypoints.min.js"></script>
+    <!-- Owlcarousel JS -->
+    <script src="vendors/owl_carousel/owl.carousel.min.js"></script>
+    <!-- Stellar JS -->
+    <script src="vendors/stellar/jquery.stellar.js"></script>
+    <!-- Theme JS -->
+    <script src="js/theme.js"></script>
+</body>
+</html>
